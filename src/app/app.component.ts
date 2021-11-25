1111f66a9b8c60544091ba8497e0403e489964e2
Amith C <amith.learning.com>
2021-11-25 13:53:28 +0530

    Flushed event-list component
    Added inline template and replaced app.component template to point to new event-list selector
    
    Added component in app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h2>Angular</h2>
-    <img src="assets/images/basic-shield.png"/>
+    <events-list></events-list>
   `
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent
   ],
   bootstrap: [AppComponent]
 })
