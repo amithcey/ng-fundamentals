36c44e66ba2e731156a851bb0e09e99c769789c9
Amith C <amith.learning.com>
2021-11-25 15:52:25 +0530

    Created thumbnail component to hold one event.
    Learned passing data from parent to child component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { EventsListComponent } from './events/events-list.component';
   ],
   declarations: [
     AppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [AppComponent]
 })
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,32 +3,22 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'events-list',
     template: `
-        <div>
-            <h1>Upcoming Angular Events</h1>
-            <hr/>
-            <div class="well hoverwell thumbnail">
-                <h2>{{event.name}}</h2>
-                <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
-                <div>Price: \${{event.price}}</div>
-                <div>
-                    <span>Location: {{event.location.address}}</span>
-                    <span>&nbsp;</span>
-                    <span>{{event.location.city}}, {{event.location.country}}</span>
-                </div>
-            </div>
-        </div>
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr />
+        <event-thumbnail [event]="event1"></event-thumbnail>
+    </div>  
     `
 })
 
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '03/11/2021',
         time: '10:00 am',
         price: 599.00,
-        imageUrl: '/assets/images/angularconnect-shield.png',
+        imageUrl: 'app/assets/images/angularconnect-shield.png',
         location: {
             address: 'No 2047, Wash Bow Bldg',
             city: 'London',
