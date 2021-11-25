f5f5359db214040a219b48fa103fe39fd82a3dda
Amith C <amith.learning.com>
2021-11-25 18:52:45 +0530

    Introduced ngFor and added safe navigators and ngIf

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,33 +1,27 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
-        </div>        
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
     </div>
     `,
     styles: [`
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb }
+        .thumbnail { min-height: 210px; }
     `
     ]
 })
 
 export class EventThumbnailComponent {
     @Input() event: any;
-    @Output() eventClicked = new EventEmitter();
-
-    name = 'Child Thumbnail';
-
-    templateVariableLogger() {
-        console.log('Parent called child using template variable')
-    }
 }
