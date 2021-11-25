7c125fe43f425d884f6c74f744572d6f5e0975c3
Amith C <amith.learning.com>
2021-11-25 19:32:54 +0530

    Using ngClass to style structural directives

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,8 @@ import { Component, Input } from "@angular/core";
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngSwitch]="event?.time">
+        <div [ngClass]="getStartTimeClass()" 
+            [ngSwitch]="event?.time">
             Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'"> (Early Start)</span>
             <span *ngSwitchCase="'10:00 am'"> (Delayed Start)</span>
@@ -21,6 +22,8 @@ import { Component, Input } from "@angular/core";
     </div>
     `,
     styles: [`
+        .green { color: #003300 !important }
+        .bold { font-weight: bold }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb }
         .thumbnail { min-height: 210px; }
@@ -30,4 +33,11 @@ import { Component, Input } from "@angular/core";
 
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeClass() {
+        if (this.event?.time == '8:00 am') {
+            return ['green', 'bold'];
+        }
+        return [];
+    }
 }
