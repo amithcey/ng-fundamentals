80ca150db14acdb35453f39ecf9ed3515fd03dad
Amith C <amith.learning.com>
2021-11-26 23:56:20 +0530

    Added guard for activated route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ToastrService } from './common/toastr.service';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/events.service';
@@ -27,7 +28,8 @@ import { appRoutes } from './nav-bar/routes';
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator
   ],
   bootstrap: [AppComponent]
 })
