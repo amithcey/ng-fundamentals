6e8503ea2859443f14d7facf1bf79cd9e5e8a38d
Amith C <amith.learning.com>
2021-11-28 15:54:26 +0530

    Added login form, used #loginForm local variable in template and ngSubmit
    Added FormsModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ProfileComponent } from "./profile.component";
 import { userRoutes } from "./user.routes";
+import { FormsModule } from "@angular/forms";
+import { LoginComponent } from "./login.component";
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild(userRoutes)
     ],
     declarations: [
-        ProfileComponent
+        ProfileComponent,
+        LoginComponent
     ],
     providers: [
 
