f6b51ccb95dccbc0251c9b0b738dd7d114c11a4d
Amith C <amith.learning.com>
2021-12-26 16:37:51 +0530

    custom validator - very useful

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { ISession } from "../shared/event.model";
+import { restrictedWords } from "../shared/restricted-words.validator";
 
 @Component({
     templateUrl: './create-session.component.html',
@@ -24,7 +25,9 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+        this.abstract = new FormControl('', [Validators.required,
+            Validators.maxLength(400),
+            restrictedWords(['foo', 'bar'])]); // this custom validator restricts words foo and bar
 
         this.newSessionForm = new FormGroup({
             name: this.name,
