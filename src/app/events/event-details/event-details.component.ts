d735250838ec598a6d4242cc630eb44f4e20d412
Amith C <amith.learning.com>
2021-12-30 18:50:39 +0530

    Filtering done in component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -16,6 +16,7 @@ import { EventService } from "../shared/events.service";
 export class EventDetailsComponent implements OnInit {
     addMode: boolean;
     event: IEvent;
+    filterBy: string = 'all';
 
     constructor(private eventService: EventService,
         private route: ActivatedRoute) { }
--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from "../shared/event.model";
 
 @Component({
@@ -6,6 +6,23 @@ import { ISession } from "../shared/event.model";
     templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[]
+    @Input() filterBy: string
+    visibleSessions: ISession[] = []
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+        }       
+    }
+
+    filterSessions(filterBy: string) {
+        if (filterBy == 'all') {
+            this.visibleSessions = this.sessions.slice(0); // this creates a copy of sessions to visible sessions
+        }
+        else {
+            this.visibleSessions = this.sessions.filter(s => s.level.toLowerCase() === filterBy);
+        }
+    }
 }
